63045c744a0bc4c7553ded4206d152002d5a5420
HALOPENE <jebory@mail.ru>
2025-01-14 22:32:13 -0500

    Начало разработки логики - Создание логики главного меню

--- app/src/main/java/com/example/mycrossandzerogame/MainActivity.kt
+++ app/src/main/java/com/example/mycrossandzerogame/MainActivity.kt
@@ -1,20 +1,59 @@
 package com.example.mycrossandzerogame
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mycrossandzerogame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.toNewGame.setOnClickListener{
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
         }
+        binding.toNewGame.setOnClickListener{
+            val data = getInfoAboutGame()
+            val intent = Intent(this, GameActivity::class.java).apply {
+                putExtra("EXTRA_TIME", data.time)
+                putExtra("EXTRA_GAME_FIELD", data.gameField)
+            }
+            startActivity(intent)
+        }
+        binding.toNewGame.setOnClickListener{
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        setContentView(binding.root)
+    }
+
+    private fun getInfoAboutGame() : InfoGame {
+        with(getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)) {
+            val time = getLong("time", 0L)
+            val gameField = getString("gameField", "")
+
+            return if(gameField != null){
+                InfoGame(time, gameField)
+            } else {
+                InfoGame(0L, "")
+            }
+        }
+    }
+    data class InfoGame(val time: Long, val gameField: String)
+
+    companion object{
+        const val EXTRA_TIME = "extra_time"
+        const val EXTRA_GAME_FIELD = "extra_game_field"
     }
 }
